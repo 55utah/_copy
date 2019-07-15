47c3102b62abc23277c2608d9299397ba0d0c7f4
shengxin <17704150+55utah@users.noreply.github.com>
2019-07-15 22:41:39 +0800

    Add files via upload

---
+++ _copy_es5.js
@@ -0,0 +1,19 @@
+//copy-es5
+function u_check(){
+    return typeof(document.execCommand) == 'function'
+}
+
+
+function u_copy(t){
+    if(!u_check()) return false
+    var model = document.createElement('textarea')
+    model.value = t
+    model.style = "position: fixed; width: 1px; height: 1px; left: -20px"
+    document.body.appendChild(model)
+    model.setSelectionRange(0, model.value.length)
+    model.focus()
+    model.select()
+    document.execCommand('copy', true)
+    document.body.removeChild(model)
+    return true
+}
